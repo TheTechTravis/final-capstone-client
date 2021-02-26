a02157ef905ab61dbac9553a56713eae08fbcece
Travis Stevenson <thetechtravis@outlook.com>
2021-02-26 14:55:34 -0600

    Removed unused code

--- src/components/task/CompletedTask.js
+++ src/components/task/CompletedTask.js
@@ -7,9 +7,5 @@ export const CompletedTask = ({ task }) => (
         <div className="task__title">Title: {task.title}</div>
         <div className="task__content">Content: {task.content}</div>
         <Button variant="danger"> Delete Task </Button>
-
-        {/* <div className="task__creationDate">Creation Date: {task.creation_date}</div> */}
-        {/* <div className="task__isComplete">Is Complete:  {task.is_complete ? "Complete" : "Incomplete"}</div> */}
-        <br />
     </section >
 )
--- src/components/task/Task.js
+++ src/components/task/Task.js
@@ -7,14 +7,10 @@ export const Task = ({ task }) => (
     < section key={`task--${task.id}`} className="taskCard" >
         <div className="task__title">Title: {task.title}</div>
         <div className="task__content">Content: {task.content}</div>
-
         <Link key={task.id} id={task.id} to={'/'} >
             <Button variant="warning"> Edit Task </Button>
         </Link>
         <Button variant="success"> Mark As Completed Task </Button>
         <Button variant="danger"> Delete Task </Button>
-        {/* <div className="task__creationDate">Creation Date: {task.creation_date}</div> */}
-        {/* <div className="task__isComplete">Is Complete:  {task.is_complete ? "Complete" : "Incomplete"}</div> */}
-        <br />
     </section >
 )
