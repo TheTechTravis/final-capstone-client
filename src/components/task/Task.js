bff495643bfbf76e1b1c2b073a981085e1cfb22b
Travis Stevenson <thetechtravis@outlook.com>
2021-02-26 12:46:55 -0600

    Added buttons to Task.js component

--- src/components/task/Task.js
+++ src/components/task/Task.js
@@ -5,8 +5,11 @@ export const Task = ({ task }) => (
     < section key={`task--${task.id}`} className="taskCard" >
         <div className="task__title">Title: {task.title}</div>
         <div className="task__content">Content: {task.content}</div>
-        <div className="task__creationDate">Creation Date: {task.creation_date}</div>
-        <div className="task__isComplete">Is Complete:  {task.is_complete ? "Complete" : "Incomplete"}</div>
+        <button type="button"> Edit Task </button>
+        <button type="button"> Mark As Completed </button>
+        <button type="button"> Delete Task </button>
+        {/* <div className="task__creationDate">Creation Date: {task.creation_date}</div> */}
+        {/* <div className="task__isComplete">Is Complete:  {task.is_complete ? "Complete" : "Incomplete"}</div> */}
         <br />
     </section >
 )
