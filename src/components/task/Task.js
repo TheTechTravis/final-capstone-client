06e34b69b04a503d873eb2aea2f1c197b28df188
Travis Stevenson <thetechtravis@outlook.com>
2021-02-26 13:39:38 -0600

    Created new components. Added react-bootstrap css import.

--- src/components/task/Task.js
+++ src/components/task/Task.js
@@ -1,13 +1,18 @@
 import React from "react"
+import { Link } from "react-router-dom"
+import Button from 'react-bootstrap/Button'
 import "./Task.css"
 
 export const Task = ({ task }) => (
     < section key={`task--${task.id}`} className="taskCard" >
         <div className="task__title">Title: {task.title}</div>
         <div className="task__content">Content: {task.content}</div>
-        <button type="button"> Edit Task </button>
-        <button type="button"> Mark As Completed </button>
-        <button type="button"> Delete Task </button>
+
+        <Link key={task.id} id={task.id} to={'/'} >
+            <Button variant="warning"> Edit Task </Button>
+        </Link>
+        <Button variant="success"> Mark As Completed Task </Button>
+        <Button variant="danger"> Delete Task </Button>
         {/* <div className="task__creationDate">Creation Date: {task.creation_date}</div> */}
         {/* <div className="task__isComplete">Is Complete:  {task.is_complete ? "Complete" : "Incomplete"}</div> */}
         <br />
